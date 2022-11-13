1482ba3cc6116d7b6116b14d2997d2d666df57e8
lkh7764 <100886992+lkh7764@users.noreply.github.com>
2022-11-13 15:27:54 +0900

    UpgradeChangePos
    Add script <ChangePos>

--- TellusCreo/Assets/Script/LKH/P_ChangePos.cs
+++ TellusCreo/Assets/Script/LKH/P_ChangePos.cs
@@ -4,26 +4,41 @@ using UnityEngine;
 
 public class P_ChangePos : MonoBehaviour
 {
-    public Vector2 beforePos;
-    Vector2 afterPos;
+    public Vector3 beforePos;
+    public Vector3 afterPos;
 
-    //기존 오브젝트 위치를 저장
+    public bool isSet = true;
+    public bool isMove = false;
+
+    void Update()
+    {
+        if (this.tag == "P_move") { isMove = true; }
+        else { isMove = false; }
+
+        if (!isMove && !isSet)
+        {
+            Debug.Log("초기화");
+            this.transform.localPosition = beforePos;
+            isSet = true;
+        }
+    }
+
     private void OnMouseDown()
     {
+        isSet = false;
         Debug.Log("위치 저장");
         beforePos = this.transform.localPosition;
     }
 
-    /*
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (other.tag == "P_stop")
+        if (!isMove)
         {
+            isSet = true;
             Debug.Log("위치 변경");
-            afterPos = other.transform.localPosition;
+            afterPos = collision.transform.localPosition;
             this.transform.localPosition = afterPos;
-            other.transform.localPosition = beforePos;
+            collision.transform.localPosition = beforePos;
         }
     }
-    */
 }
