7ee5a30892ed4ff4776b17a3c907ce1c1b384cc8
lkh7764 <100886992+lkh7764@users.noreply.github.com>
2022-11-19 19:50:20 +0900

    Complete_ChangePos
    Complete script<ChangePos>

--- TellusCreo/Assets/Script/LKH/P_ChangePos.cs
+++ TellusCreo/Assets/Script/LKH/P_ChangePos.cs
@@ -7,44 +7,110 @@ public class P_ChangePos : MonoBehaviour
     public Vector2 beforePos;
     public Vector2 afterPos;
 
-    public bool isSet = true;
     public bool isMove = false;
+    public bool isSet = true;
+    public bool startOnTrig = false;
 
-    void Update()
+    private int layerNum;
+    private int layer_S;
+    private int layer_NS;
+    public int checkLayer;
+
+
+    private void Start()
+    {
+        GetComponent<Collider2D>().isTrigger = true;
+        layer_S = SortingLayer.NameToID("Select");
+        layer_NS = SortingLayer.NameToID("NotSelect");
+    }
+
+    IEnumerator StartSet()
+    {
+        yield return null;
+        SetObj();
+        Debug.Log("SetObj 실행");
+    }
+
+    private void FixedUpdate()
+    {
+        if (startOnTrig)
+        {
+            StartCoroutine(StartSet());
+        }
+    }
+
+    private void Update()
     {
         if (this.tag == "P_move") { isMove = true; }
         else { isMove = false; }
+    }
 
-        if (!isMove && !isSet)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isSet && collision.tag == "P_stop")
         {
-            Debug.Log("초기화");
-            this.transform.localPosition = beforePos;
-            isSet = true;
+            afterPos = collision.transform.localPosition;
+            collision.transform.localPosition = beforePos;
+            beforePos = afterPos;
+            if (beforePos == afterPos) { Debug.Log("collision위치 저장 완료"); }
+
+            SetObj();
         }
     }
 
     private void OnMouseDown()
     {
         isSet = false;
-        GetComponent<Collider2D>().isTrigger = false;
-        Debug.Log("위치 저장");
         beforePos = this.transform.localPosition;
+
+        layerNum = 9;
+        ChangeLayer();
+        checkLayer = 1;
     }
 
     private void OnMouseExit()
     {
-        GetComponent<Collider2D>().isTrigger = true;
+        if (checkLayer == 1 && !isMove)
+        {
+            layerNum = 8;
+            ChangeLayer();
+            checkLayer = 2;
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void ChangeLayer()
     {
-        if (!isMove)
+        if (layerNum == 8)
         {
+            this.gameObject.layer = 8;
+            GetComponent<SpriteRenderer>().sortingLayerID = layer_NS;
+            if (this.gameObject.layer == 8) { Debug.Log("레이어변경: 8"); }
+        }
+        else if (layerNum == 9)
+        {
+            this.gameObject.layer = 9;
+            GetComponent<SpriteRenderer>().sortingLayerID = layer_S;
+            if (this.gameObject.layer == 9) { Debug.Log("레이어변경: 9"); }
+        }
+    }
+
+    public void SetObj()
+    {
+        if (!isSet && !isMove)
+        {
+            this.transform.localPosition = beforePos;
             isSet = true;
-            Debug.Log("위치 변경");
-            afterPos = collision.transform.localPosition;
-            this.transform.localPosition = afterPos;
-            collision.transform.localPosition = beforePos;
+            startOnTrig = false;
+            checkLayer = 0;
+            Debug.Log("위치초기화");
+        }
+    }
+
+    public void LateUpdate()
+    {
+        if (checkLayer == 2)
+        {
+            startOnTrig = true;
         }
     }
 }
