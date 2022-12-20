7b036c0fd3f42fdbcf3c8151dc91414761827157
Leejunghan1148 <junghan1148@gmail.com>
2022-12-20 17:55:23 +0900

    다른 프로젝트에 한 것 옮김

---
+++ TellusCreo/Assets/Script/LJH/TextFolder.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextFolder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool state = false;
+   
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+
+
+    //}
+
+    
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("StartFolder"))
+        {
+            state = false;
+           // Debug.Log("꺼졌어요");
+
+        }
+
+        if (collision.CompareTag("TextFolder"))
+        {
+            if (state == true)
+            {
+                collision.gameObject.GetComponent<TextFolder>().state = false;
+                //Debug.Log("bad");
+            }
+        }
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("StartFolder"))
+        {
+            state = true;
+            //Debug.Log("우왕");
+           
+        }
+        
+
+        //if (collision.CompareTag("TextFolder"))
+        //{
+        //    if (state == true)
+        //    {
+        //        collision.gameObject.GetComponent<TextFolder>().state = true;
+        //        Debug.Log("good");
+        //    }
+        //}
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("TextFolder"))
+        {
+            if (state == true)
+            {
+                collision.gameObject.GetComponent<TextFolder>().state = true;
+                //Debug.Log("good");
+            }
+        }
+
+    }
+
+    
+
+
+}
