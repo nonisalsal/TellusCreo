a75bdc182b6519f2f51013f669233e38ea22fc55
cowkjw <cowkjw@naver.com>
2022-12-22 16:42:10 +0900

    12.22

--- TellusCreo/Assets/Script/KJW/Poster.cs
+++ TellusCreo/Assets/Script/KJW/Poster.cs
@@ -6,10 +6,17 @@ public class Poster : MonoBehaviour
 {
     LineRenderer _lr;
     [SerializeField]
-    int _idx;
+    int _prevIdx;
+    int _lineIdx;
+    bool isStart;
+    Vector2 _prevPos;
     void Start()
     {
-        _idx = 0;
+        isStart = false;
+        _lineIdx = 0;
+        _prevIdx = 0;
+
+        _lr = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
@@ -23,17 +30,28 @@ public class Poster : MonoBehaviour
                 return;
             if (hit.collider.gameObject.layer == 8) // 레이어 8 (point인지)
             {
-                //if (transform.GetChild(_idx).GetComponent<PosterPoint>().isClick)
-                //{
-                //   // transform.GetChild(_idx).GetComponent<PosterPoint>().PointEnabled();
-                //}
-                //else
-                //{
-                    hit.collider.gameObject.GetComponent<PosterPoint>().PointEnabled();
-                    _idx = hit.collider.transform.GetSiblingIndex();
-              //  }
+                if (!isStart)
+                {
 
-              
+                    _prevIdx = hit.collider.transform.GetSiblingIndex(); // 이전 선택 인덱스
+                    isStart = true; // 시작퍼즐 판단
+                    _lr.positionCount++; // 라인렌더러 ++
+                    _lr.SetPosition(_lineIdx++, hit.collider.transform.position);
+                    hit.collider.gameObject.GetComponent<PosterPoint>().PointEnabled();
+                }
+                else
+                {
+                    if (_prevIdx != hit.collider.transform.GetSiblingIndex())
+                    {
+
+                        _lr.positionCount++;
+                        _lr.SetPosition(_lineIdx++, hit.collider.transform.position);
+                        hit.collider.gameObject.GetComponent<PosterPoint>().PointEnabled();
+                        //   hit.collider.gameObject.GetComponent<PosterPoint>().node.Add(_prevIdx); // 시작 위치의 노드를 리스트에 넣음
+
+                        isStart = false;
+                    }
+                }
             }
         }
         else if (Input.GetMouseButtonDown(1))
@@ -44,7 +62,9 @@ public class Poster : MonoBehaviour
                 if (transform.GetChild(i).GetComponent<PosterPoint>().isClick)
                     transform.GetChild(i).GetComponent<PosterPoint>().PointEnabled();
             }
-            _idx = 0;
+            _lr.positionCount = 0;
+            _lineIdx = 0;
+            _prevIdx = 0;
         }
     }
 }
