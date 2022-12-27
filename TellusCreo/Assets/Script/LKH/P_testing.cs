93de4c516179357f7eee534351eb657bdcf77da7
lkh7764 <100886992+lkh7764@users.noreply.github.com>
2022-12-27 16:18:01 +0900

    2022_12_27
    -edit <P_TowerPuzzle>
    -<P_testing>: camera script
    -add <P_PuzzleObject>

--- TellusCreo/Assets/Script/LKH/P_testing.cs
+++ TellusCreo/Assets/Script/LKH/P_testing.cs
@@ -4,121 +4,20 @@ using UnityEngine;
 
 public class P_testing : MonoBehaviour
 {
-    private int layer_S;
-    private int layer_NS;
+    public bool playPuzzle = false;
+    public float puzzlePos_x;
+    public float puzzlePos_y;
 
-    public bool isRight;
-
-    public Vector2 beforePos, afterPos;
-    private struct AfterPositions
+    private void Start()
     {
-        float afterX;
-        float afterY;
-    }
-
-    private Rigidbody2D rig;
-
-    private void OnMouseDown()
-    {
-        layer_S = SortingLayer.NameToID("P_Select");
-        layer_NS = SortingLayer.NameToID("P_NotSelect");
-        ChangeLayer(8);
-        isRight = false;
-        this.transform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
+        //playPuzzle = false;
     }
 
     private void Update()
     {
-        if(this.transform.position.y >= 4)
+        if (playPuzzle == true)
         {
-            isRight = true;
-        }
-        if(isRight==true &&this.transform.position.y < 4)
-        {
-            isRight = false;
-        }
-    }
-
-    private void OnMouseDrag()
-    {
-        this.transform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
-    }
-
-    private void OnMouseUp()
-    {
-        ChangeLayer(8);
-        if (this.transform.position.y < beforePos.y)
-        {
-            this.transform.position = beforePos;
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.name == "platform")
-        {
-            if (collision.contacts[0].normal.y > 0.7f)
-            {
-                beforePos = this.transform.position;
-            }
-        }
-    }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.gameObject.name == "platform" )
-        {
-            if (collision.contacts[0].normal.y > 0.7f)
-            {
-                this.tag = "P_building";
-            }
-            else
-            {
-                if (collision.contacts[0].normal.x <= 0.7f)
-                {
-                    this.transform.position = beforePos;
-                }
-            }
-        }
-
-        if (collision.gameObject.name == "wall")
-        {
-            if (collision.contacts[0].normal.x <= 0.7f)
-            {
-                this.transform.position = beforePos;
-            }
-        }
-
-        if (collision.gameObject.tag == "P_building")
-        {
-            if (collision.contacts[0].normal.y < 0.7f)
-            {
-                if (collision.contacts[0].normal.x <= 0.7f)
-                {
-                    this.transform.position = beforePos;
-                }
-            }
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        this.tag = "P_stop";
-    }
-
-    private void ChangeLayer(int layerNum)
-    {
-        if (layerNum == 8)
-        {
-            this.gameObject.layer = 8;
-            GetComponent<SpriteRenderer>().sortingLayerID = layer_NS;
-            //if (this.gameObject.layer == 8) { Debug.Log("레이어변경: 8"); }
-        }
-        else if (layerNum == 9)
-        {
-            this.gameObject.layer = 9;
-            GetComponent<SpriteRenderer>().sortingLayerID = layer_S;
-            //if (this.gameObject.layer == 9) { Debug.Log("레이어변경: 9"); }
+            this.transform.position = new Vector3(puzzlePos_x, puzzlePos_y, -10);
         }
     }
 }
