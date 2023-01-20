d543388193c591d23a67ab7118ee58ce8bedd615
Leejunghan1148 <junghan1148@gmail.com>
2023-01-20 14:01:39 +0900

    0120

---
+++ TellusCreo/Assets/Script/LJH/Trophy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trophy : MonoBehaviour
+{
+   
+    public bool check1;
+
+    void Start()
+    {
+        check1 = false;
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+
+
+        if (collider.gameObject.name == "trophy")
+        {
+            check1 = true;
+            //Debug.Log(1);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collider)
+    {
+
+        if (collider.gameObject.name == "trophy")
+        {
+            check1 = false;
+            //Debug.Log(0);
+        }
+    }
+    
+}
