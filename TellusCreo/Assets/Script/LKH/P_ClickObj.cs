46d983855eec167ce1d8cce4e977b6c7d20a38f4
lkh7764 <100886992+lkh7764@users.noreply.github.com>
2023-02-14 16:10:55 +0900

    20230214
    0214

--- TellusCreo/Assets/Script/LKH/P_ClickObj.cs
+++ TellusCreo/Assets/Script/LKH/P_ClickObj.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class P_ClickObj : MonoBehaviour
 {
-    public GameObject rayControl;
+    private bool isUp;
+    private Ray2D upRay;
+    private RaycastHit2D upHit;
 
     private bool[] getItem;
     private bool getKey_A;
@@ -16,43 +18,59 @@ public class P_ClickObj : MonoBehaviour
     void Start()
     {
         getItem = new bool[8] { false, false, false, false, false, false, false, false };
+        //for (int i=0; i<8; i++) { Debug.Log(this.transform.GetChild(i).gameObject.name); }
     }
 
     void Update()
     {
-        if (FindObjectOfType<P_Camera>().playPuzzle == false)
+        //if (FindObjectOfType<P_Camera>().playPuzzle == false)
+        //{
+        //    ShootRay();
+        //    PlayerInput();
+        //}
+        ShootRay();
+        PlayerInput();
+    }
+
+    private void ShootRay()
+    {
+        if (Input.GetMouseButtonUp(0))
         {
-            PlayerInput();
+            isUp = true;
+            Vector2 upPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            upRay = new Ray2D(upPos, Vector2.zero);
+            upHit = Physics2D.Raycast(upRay.origin, upRay.direction);
         }
+        else { isUp = false; }
     }
 
     private void PlayerInput()
     {
-        if (rayControl.GetComponent<P_GameManager>().isUp == true)
+        if (isUp)
         {
-            RaycastHit2D upHit = rayControl.GetComponent<P_GameManager>().upHit;
             if (upHit)
             {
-                if (getItem[0] == false && System.Object.ReferenceEquals(this.transform.GetChild(0), upHit.collider.gameObject.transform))
+                if (getItem[0] == false && System.Object.ReferenceEquals(this.transform.GetChild(0).gameObject, upHit.collider.gameObject))
                 {
                     if (!getKey_A) { Debug.Log("Need 'Key_A'"); }
                     else { 
                         Debug.Log("Get 'Wire'");
+                        getWire = true;
                         getItem[0] = true;
                     }
                 }
-                if (getItem[1] == false && System.Object.ReferenceEquals(this.transform.GetChild(1), upHit.collider.gameObject.transform))
+                if (getItem[1] == false && System.Object.ReferenceEquals(this.transform.GetChild(1).gameObject, upHit.collider.gameObject))
                 {
                     Debug.Log("Get 'Guitar'");
                     getItem[1] = true;
                 }
-                if (getItem[2] == false && System.Object.ReferenceEquals(this.transform.GetChild(2), upHit.collider.gameObject.transform))
+                if (getItem[2] == false && System.Object.ReferenceEquals(this.transform.GetChild(2).gameObject, upHit.collider.gameObject))
                 {
                     Debug.Log("Get 'Key_A'");
                     getKey_A = true;
                     getItem[2] = true;
                 }
-                if (getItem[3] == false && System.Object.ReferenceEquals(this.transform.GetChild(3), upHit.collider.gameObject.transform))
+                if (getItem[3] == false && System.Object.ReferenceEquals(this.transform.GetChild(3).gameObject, upHit.collider.gameObject))
                 {
                     if (!getPattern) { Debug.Log("Need 'Pattern'"); }
                     else
@@ -61,12 +79,12 @@ public class P_ClickObj : MonoBehaviour
                         getItem[3] = true;
                     }
                 }
-                if (getItem[4] == false && System.Object.ReferenceEquals(this.transform.GetChild(4), upHit.collider.gameObject.transform))
+                if (getItem[4] == false && System.Object.ReferenceEquals(this.transform.GetChild(4).gameObject, upHit.collider.gameObject))
                 {
                     Debug.Log("Get 'Violin'");
                     getItem[4] = true;
                 }
-                if (getItem[5] == false && System.Object.ReferenceEquals(this.transform.GetChild(5), upHit.collider.gameObject.transform))
+                if (getItem[5] == false && System.Object.ReferenceEquals(this.transform.GetChild(5).gameObject, upHit.collider.gameObject))
                 {
                     if (!contectWire) { Debug.Log("Need 'Contect Wire'"); }
                     else
@@ -75,7 +93,7 @@ public class P_ClickObj : MonoBehaviour
                         getItem[5] = true;
                     }
                 }
-                if (getItem[6] == false && System.Object.ReferenceEquals(this.transform.GetChild(6), upHit.collider.gameObject.transform))
+                if (getItem[6] == false && System.Object.ReferenceEquals(this.transform.GetChild(6).gameObject, upHit.collider.gameObject))
                 {
                     if (!getKey_B) { Debug.Log("Need 'Key_B'"); }
                     else
@@ -84,12 +102,13 @@ public class P_ClickObj : MonoBehaviour
                         getItem[6] = true;
                     }
                 }
-                if (getItem[7] == false && System.Object.ReferenceEquals(this.transform.GetChild(7), upHit.collider.gameObject.transform))
+                if (getItem[7] == false && System.Object.ReferenceEquals(this.transform.GetChild(7).gameObject, upHit.collider.gameObject))
                 {
                     if (!getWire) { Debug.Log("Need 'Wire'"); }
                     else
                     {
                         Debug.Log("Use 'Lamp'");
+                        contectWire = true;
                         getItem[7] = true;
                     }
                 }
