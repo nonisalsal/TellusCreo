b169756665b74baa0e2e2f318a6208c010cb9f27
cowkjw <cowkjw@naver.com>
2023-03-09 20:58:51 +0900

    03.09
    자물쇠 구현

---
+++ TellusCreo/Assets/Script/KJW/Puzzles/PasswordController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PasswordController : MonoBehaviour
+{
+    [SerializeField]
+    Text passwordText;
+    string password = "0";
+    public string Password { get => password; set => ChangePassword(value); }
+
+    void ChangePassword(string val)
+    {
+        int tempPassword = (int.Parse(password) + int.Parse(val) + 10 )% 10;
+        password = tempPassword.ToString();
+
+        passwordText.text = password;
+    }
+}
