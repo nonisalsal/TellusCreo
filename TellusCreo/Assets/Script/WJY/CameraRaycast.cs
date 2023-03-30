ee971c63b30fb356310b63873532d7551dcd9607
dnwodud2579 <dnwodud2579@gmail.com>
2023-03-30 19:33:09 +0900

    2023.03.30
    2023.03.30

---
+++ TellusCreo/Assets/Script/WJY/CameraRaycast.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraRaycast : MonoBehaviour
+{
+    RaycastHit hit;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (EventSystem.current.IsPointerOverGameObject() == false)
+                {
+                    Debug.DrawRay(ray.origin, ray.direction * 1000, Color.blue);
+                }
+            }
+        }
+    }
+}
