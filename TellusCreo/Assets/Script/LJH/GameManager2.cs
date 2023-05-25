d9d16dec16c17ca4a0324e146a9b0be0ba854e35
Leejunghan1148 <junghan1148@gmail.com>
2023-05-25 20:27:03 +0900

    5.25

--- TellusCreo/Assets/Script/LJH/GameManager2.cs
+++ TellusCreo/Assets/Script/LJH/GameManager2.cs
@@ -1,48 +1,90 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.Experimental.Rendering.Universal;
+
 
 public class GameManager2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-   
-    public string targetTag = "Clickable"; // 클릭 가능한 게임 오브젝트의 태그
-    public float moveSpeed = 5f; // 카메라 이동 속도
+    static GameManager2 s_instance = null;
+    public static GameManager2 Instance { get { if (s_instance == null) return null; return s_instance; } }
 
-    private Transform mainCameraTransform;
-    private bool isMovingCamera;
+    public enum Puzzle
+    {
+        HiddenHandWriting = 10,
+        ComputerPassword,
+    }
+
+
+    
+
+    // Start is called before the first frame update
+
+    //public string targetTag = "Clickable"; // 클릭 가능한 게임 오브젝트의 태그
+    //public float moveSpeed = 5f; // 카메라 이동 속도
+
+    //private Transform mainCameraTransform;
+    //private bool isMovingCamera;
 
     private void Start()
     {
-        mainCameraTransform = Camera.main.transform;
-        isMovingCamera = false;
+        //mainCameraTransform = Camera.main.transform;
+        //isMovingCamera = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
 
-            if (hit.collider != null && hit.collider.CompareTag(targetTag))
-            {
-                isMovingCamera = true;
-            }
+        if (!EventSystem.current.IsPointerOverGameObject()){
+
         }
 
-        if (isMovingCamera)
+        void HandlePuzzleClick()
         {
-            Vector3 targetPosition = mainCameraTransform.position;
-            targetPosition.x = transform.position.x;
-            targetPosition.y = transform.position.y;
-            mainCameraTransform.position = Vector3.MoveTowards(mainCameraTransform.position, targetPosition, moveSpeed * Time.deltaTime);
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D rayHit = Physics2D.Raycast(mousePos, transform.forward, 10f);
+            if (rayHit.collider == null)
+                return;
 
-            if (mainCameraTransform.position == targetPosition)
+            if (Input.GetMouseButtonDown(0))
             {
-                isMovingCamera = false;
+                GameObject hitGameObject = rayHit.collider.gameObject;
+
+                //switch ((Puzzle)[(int)hitGameObject.LightSwitch - 10])
+                //{
+
+                //}
             }
+
+
         }
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+        //    if (hit.collider != null && hit.collider.CompareTag(targetTag))
+        //    {
+        //        isMovingCamera = true;
+        //    }
+        //}
+
+        //if (isMovingCamera)
+        //{
+        //    Vector3 targetPosition = mainCameraTransform.position;
+        //    targetPosition.x = transform.position.x;
+        //    targetPosition.y = transform.position.y;
+        //    mainCameraTransform.position = Vector3.MoveTowards(mainCameraTransform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        //    if (mainCameraTransform.position == targetPosition)
+        //    {
+        //        isMovingCamera = false;
+        //    }
+        //}
     }
 }
