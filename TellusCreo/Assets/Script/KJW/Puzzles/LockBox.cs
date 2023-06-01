5687397f4a29ecfd54d2968ee87d1d422e0d2425
cowkjw <cowkjw@naver.com>
2023-06-01 20:16:55 +0900

    자물쇠 상자 리소스, 스크립트 추가

---
+++ TellusCreo/Assets/Script/KJW/Puzzles/LockBox.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LockBox : MonoBehaviour
+{
+
+    enum LockBoxType
+    {
+        Open,
+        Close,
+        Unlock,
+        Padlock
+    }
+
+    public bool IsUnlock = false;
+    [SerializeField]
+    private GameObject Lock;
+    [SerializeField]
+    private Dictionary<LockBoxType, Sprite> LockBoxBackgrounds;
+    [SerializeField]
+    private GameObject _rewardBall;
+    private SpriteRenderer _spriteRenderer;
+
+    void OnMouseDown()
+    {
+        if (!IsUnlock)
+        {
+            _spriteRenderer.sprite = LockBoxBackgrounds[LockBoxType.Padlock];
+            Lock?.SetActive(true);
+        }
+        else
+        {
+            _rewardBall?.SetActive(true);
+            _spriteRenderer.sprite = LockBoxBackgrounds[LockBoxType.Open];
+        }
+    }
+    private void OnEnable()
+    {
+        if (_spriteRenderer == null|| LockBoxBackgrounds ==null)
+        {
+            return;
+        }
+
+        if (Lock != null && Lock.activeSelf) // null이 아니고 켜져있었다면 끄기
+        {
+            Lock.SetActive(false);
+        }
+
+        if (!IsUnlock && LockBoxBackgrounds.ContainsKey(LockBoxType.Close))
+        {
+            _spriteRenderer.sprite = LockBoxBackgrounds[LockBoxType.Close];
+        }
+        else if (IsUnlock && LockBoxBackgrounds.ContainsKey(LockBoxType.Unlock))
+        {
+            _spriteRenderer.sprite = LockBoxBackgrounds[LockBoxType.Unlock];
+        }
+    }
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        LockBoxBackgrounds = new Dictionary<LockBoxType, Sprite>();
+        LockBoxBackgrounds.Add(LockBoxType.Open, Resources.Load<Sprite>("Sprites/KJW/puzzle_Box_Open"));
+        LockBoxBackgrounds.Add(LockBoxType.Close, Resources.Load<Sprite>("Sprites/KJW/puzzle_Box_Lock"));
+        LockBoxBackgrounds.Add(LockBoxType.Unlock, Resources.Load<Sprite>("Sprites/KJW/puzzle_Box_Unlock"));
+        LockBoxBackgrounds.Add(LockBoxType.Padlock, Resources.Load<Sprite>("Sprites/KJW/Padlock"));
+#if UNITY_EDITOR
+        Debug.Log(LockBoxBackgrounds.Count);
+#endif
+    }
+
+}
