b301cfafd2adbd49e194f3de556ff9d18ab248d9
lkh7764 <100886992+lkh7764@users.noreply.github.com>
2023-06-22 20:29:51 +0900

    20230622
    >작업 내용
    - 장난감상자 오류 수정
    - 장난감상자 클리어 이후 기능 구현 완료
    
    > 작업 하던 것
    - DollPuzzle2 script

--- TellusCreo/Assets/Script/LKH/LKH_PuzzleScript/P_ClickItem.cs
+++ TellusCreo/Assets/Script/LKH/LKH_PuzzleScript/P_ClickItem.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 public class P_ClickItem : MonoBehaviour
 {
     public GameObject rayControl;
+    private bool toybox;
+    private P_PuzzleObject toy_obj;
+    private GameObject toy_after;
+
+    private void Start()
+    {
+        if (this.name == "puzzle_toybox_cover")
+        {
+            toybox = true;
+            toy_obj = GameObject.Find("toy_box").GetComponent<P_PuzzleObject>();
+            toy_after = GameObject.Find("P_Interaction").transform.GetChild(2).gameObject;
+        }
+        else
+            toybox = false;
+    }
 
     void Update()
     {
@@ -18,9 +33,18 @@ public class P_ClickItem : MonoBehaviour
             RaycastHit2D upHit = rayControl.GetComponent<P_GameManager>().upHit;
             if (System.Object.ReferenceEquals(this.gameObject, upHit.collider.gameObject))
             {
-                Debug.Log("Get Spin A");
-                // 인벤토리
-                Destroy(this.gameObject);
+                if (toybox)
+                {
+                    toy_after.SetActive(true);
+                    toy_obj.puzzleClear = toy_after;
+                    Destroy(GameObject.Find("ToyBoxClear"));
+                }
+                else
+                {
+                    Debug.Log("Get Spin A");
+                    // 인벤토리
+                    Destroy(this.gameObject);
+                }
             }
         }
     }
