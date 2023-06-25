5d5ed42d1a39937dfb82b6dec6bd21f965e3e799
cowkjw <cowkjw@naver.com>
2023-06-25 16:13:49 +0900

    06.25
    패키지 추가

--- TellusCreo/Assets/Script/WJY/PickUpItem.cs
+++ TellusCreo/Assets/Script/WJY/PickUpItem.cs
@@ -51,6 +51,8 @@ public class PickUpItem : MonoBehaviour
                 Vector3 forward = transform.TransformDirection(Vector3.forward) * 5;
                 
             }
+            
+
         }
     }
     void ItemPickUp()
