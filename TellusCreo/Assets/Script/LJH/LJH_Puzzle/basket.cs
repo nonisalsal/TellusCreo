4fcf8d0fa5a89621c99dfc7b8127825cb6762133
Leejunghan1148 <junghan1148@gmail.com>
2023-07-23 21:56:04 +0900

    0723

--- TellusCreo/Assets/Script/LJH/LJH_Puzzle/basket.cs
+++ TellusCreo/Assets/Script/LJH/LJH_Puzzle/basket.cs
@@ -20,7 +20,7 @@ public class basket : MonoBehaviour
         if (collider.gameObject.name == "basket")
         {
             check4 = true;
-            Debug.Log("bas");
+            //Debug.Log("bas");
         }
     }
 
