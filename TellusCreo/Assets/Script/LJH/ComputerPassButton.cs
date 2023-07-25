e11a69882aaac80442ce373f2d0c1fd0ce14d93d
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-07-26 00:13:28 +0900

    패키지 추가
    인벤토리, 서재 패키지 추가 0726

--- TellusCreo/Assets/Script/LJH/ComputerPassButton.cs
+++ TellusCreo/Assets/Script/LJH/ComputerPassButton.cs
@@ -7,7 +7,7 @@ public class ComputerPassButton : MonoBehaviour
     public Vector3 position1 = new Vector3(0, 0, 0);
     public Vector3 position2 = new Vector3(0, 0, 1);
     public Vector3 position3 = new Vector3(1, 0, 0);
-    public Vector3 position4 = new Vector3(1, 0, 1);
+   
     public GameObject objectToCreate;
 
     private void OnMouseDown()
@@ -24,10 +24,7 @@ public class ComputerPassButton : MonoBehaviour
         {
             Instantiate(objectToCreate, position3, Quaternion.identity);
         }
-        else if (!GameObjectAtPosition(position4))
-        {
-            Instantiate(objectToCreate, position4, Quaternion.identity);
-        }
+       
     }
 
    
