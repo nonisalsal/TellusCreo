199eddc02854b261a716c07fcdb2c553c73af0c3
dnwodud2579 <dnwodud2579@gmail.com>
2023-09-07 10:50:30 +0900

    2023-09-07
    save

--- TellusCreo/Assets/Script/WJY/new/Item.cs
+++ TellusCreo/Assets/Script/WJY/new/Item.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 [CreateAssetMenu(fileName = "New Item", menuName = "Item/Create New Item")]
+
 public class Item : ScriptableObject
 {
     public string itemName;
