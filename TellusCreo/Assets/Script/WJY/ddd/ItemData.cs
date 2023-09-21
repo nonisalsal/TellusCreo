0f7eda904f77d4229f44e72aaec7dbab4c1eae9a
dnwodud2579 <dnwodud2579@gmail.com>
2023-09-21 16:11:46 +0900

    2023-09-21

---
+++ TellusCreo/Assets/Script/WJY/ddd/ItemData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ItemData
+{
+    public string itemName;
+    public Sprite itemImage;
+
+    public ItemData(Item item)
+    {
+        itemName = item.itemName;
+        itemImage = item.icon;
+    }
+}
