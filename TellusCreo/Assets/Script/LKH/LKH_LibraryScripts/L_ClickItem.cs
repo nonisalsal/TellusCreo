22eaffe8b21eaf053ff8901c07b0625b3b19dc1c
lkh7764 <100886992+lkh7764@users.noreply.github.com>
2023-09-21 16:13:47 +0900

    20230921

--- TellusCreo/Assets/Script/LKH/LKH_LibraryScripts/L_ClickItem.cs
+++ TellusCreo/Assets/Script/LKH/LKH_LibraryScripts/L_ClickItem.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class L_ClickItem : MonoBehaviour
 {
+    [SerializeField] private bool hasPair = false;
+
     private void OnDestroy()
     {
         Debug.Log("asdf");
         SoundManager.Instance.Play("item_get");
+
+        if (hasPair)
+        {
+            transform.parent.gameObject.SetActive(false);
+        }
     }
 }
