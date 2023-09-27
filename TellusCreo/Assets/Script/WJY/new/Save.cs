2bc41be2a3916f2fbd26f22a6a0b1c71410192a9
dnwodud2579 <dnwodud2579@gmail.com>
2023-09-27 18:16:48 +0900

    2023.09.27
    Save시스템 수정,ui 추가

---
+++ TellusCreo/Assets/Script/WJY/new/Save.cs
@@ -0,0 +1,33 @@
+﻿using SaveIsEasy;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Save : MonoBehaviour
+{
+
+    public bool Open = true;
+
+    //to avoid setting an empty name we use sceneFileName variable
+    private string sceneFileName;
+    private Scene selected;
+
+    private void Start()
+    {
+        selected = SceneManager.GetSceneAt(0);
+        sceneFileName = SaveIsEasyAPI.GetSceneFileNameByScene(selected);
+
+
+    }
+
+    public void save()
+    {
+        SaveIsEasyAPI.SaveAll(selected);
+    }
+    public void load()
+    {
+
+        SaveIsEasyAPI.LoadAll(selected);
+    }
+
+}
