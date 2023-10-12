c57dcfd98910ac04abd3c322b21c34b986179162
dnwodud2579 <dnwodud2579@gmail.com>
2023-10-12 23:46:42 +0900

    2023-10-21
    기존 save 시스템 삭제,
    더 쉬운 방법으로 save 시스템 설계 및 추가

--- TellusCreo/Assets/Script/KJW/Title/TitleButton.cs
+++ TellusCreo/Assets/Script/KJW/Title/TitleButton.cs
@@ -5,18 +5,37 @@ using UnityEngine.UI;
 public class TitleButton : MonoBehaviour
 {
     [SerializeField] private Button gameStartButton;
+
     private void Awake()
     {
         EarthMaterial earthMaterial = EarthMaterial.GetInstance();
+
         if (!earthMaterial.GetcutValue())
         {
-            gameStartButton.onClick.AddListener(() => SceneManager.LoadScene("TitleCutscene"));
-            earthMaterial.SetcutValue(true);
+            gameStartButton.onClick.AddListener(() => {
+                SceneManager.LoadScene("TitleCutscene");
+                earthMaterial.SetcutValue(true);
+            });
         }
         else
         {
-            gameStartButton.onClick.AddListener(() => SceneManager.LoadScene("livingroom"));
+         
+            if (earthMaterial.GetSoilValue() && earthMaterial.GetWaterValue())
+            {
+                
+                // 라이브러리룸으로 이동
+                // SceneManager.LoadScene("Libraryroom");
+            }
+            else if (earthMaterial.GetSoilValue())
+            {
+                // Soil만 true인 경우
+                // 애틱으로 이동
+                SceneManager.LoadScene("Attic");
+            }
+            else
+            {
+                SceneManager.LoadScene("livingroom");
+            }
         }
-
     }
 }
--- TellusCreo/Assets/Script/WJY/new/Save.cs
+++ TellusCreo/Assets/Script/WJY/new/Save.cs
@@ -1,33 +1,38 @@
-﻿using SaveIsEasy;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-
 
 public class Save : MonoBehaviour
 {
-
-    public bool Open = true;
-
-    //to avoid setting an empty name we use sceneFileName variable
-    private string sceneFileName;
-    private Scene selected;
-
     private void Start()
     {
-        selected = SceneManager.GetSceneAt(0);
-        sceneFileName = SaveIsEasyAPI.GetSceneFileNameByScene(selected);
-
-
+        // 게임 시작 시, 불러오기 함수를 호출하여 데이터를 불러옵니다.
+        
     }
 
     public void save()
     {
-        SaveIsEasyAPI.SaveAll(selected);
+        // 각 변수를 PlayerPrefs에 저장합니다.
+        PlayerPrefs.SetInt("Sun", EarthMaterial.GetInstance().GetSunValue() ? 1 : 0);
+        PlayerPrefs.SetInt("Water", EarthMaterial.GetInstance().GetWaterValue() ? 1 : 0);
+        PlayerPrefs.SetInt("Soil", EarthMaterial.GetInstance().GetSoilValue() ? 1 : 0);
+        PlayerPrefs.SetInt("CutS", EarthMaterial.GetInstance().GetcutValue() ? 1 : 0);
+
+        PlayerPrefs.Save(); // 저장을 확실히 하려면 Save() 메서드를 호출
     }
-    public void load()
+
+    public void Load()
     {
+        // PlayerPrefs에서 데이터를 불러와서 게임 상태를 업데이트합니다.
+        bool sunValue = PlayerPrefs.GetInt("Sun", 0) == 1; // 0은 기본값 (false)
+        EarthMaterial.GetInstance().SetSunValue(sunValue);
 
-        SaveIsEasyAPI.LoadAll(selected);
+        bool waterValue = PlayerPrefs.GetInt("Water", 0) == 1;
+        EarthMaterial.GetInstance().SetWaterValue(waterValue);
+
+        bool soilValue = PlayerPrefs.GetInt("Soil", 0) == 1;
+        EarthMaterial.GetInstance().SetSoilValue(soilValue);
+
+        bool cutValue = PlayerPrefs.GetInt("CutS", 0) == 1;
+        EarthMaterial.GetInstance().SetcutValue(cutValue);
     }
-
 }
