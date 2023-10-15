66aad845d290f50b4365dcebb7247c961427517d
dnwodud2579 <dnwodud2579@gmail.com>
2023-10-15 22:42:03 +0900

    2023-10-15
    스크립트 수정 및 세이브 수정

--- TellusCreo/Assets/Script/WJY/new/SetEarth.cs
+++ TellusCreo/Assets/Script/WJY/new/SetEarth.cs
@@ -5,7 +5,21 @@ public class SetEarth : MonoBehaviour
     public GameObject Sunobj;
     public GameObject Soilobj;
     public GameObject Waterobj;
+
+    public bool sunbool;
+    public bool soilobj;
+    public bool waterbool;
     void Start()
+    {
+        UpdateEarthObjects();
+    }
+
+    private void OnEnable()
+    {
+        UpdateEarthObjects();
+    }
+
+    public void UpdateEarthObjects()
     {
         EarthMaterial earthMaterial = EarthMaterial.GetInstance();
 
@@ -13,13 +27,28 @@ public class SetEarth : MonoBehaviour
         {
             Soilobj.SetActive(true);
         }
+        else
+        {
+            Soilobj.SetActive(false);
+        }
+
         if (earthMaterial.GetSunValue())
         {
             Sunobj.SetActive(true);
         }
+        else
+        {
+            Sunobj.SetActive(false);
+        }
+
         if (earthMaterial.GetWaterValue())
         {
             Waterobj.SetActive(true);
         }
+        else
+        {
+            Waterobj.SetActive(false);
+        }
     }
 }
+
